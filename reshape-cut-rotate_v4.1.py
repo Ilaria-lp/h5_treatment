075b3a36ecc01a011b278b65ef2abbaea64ae2e1
Ilaria-lp <108761035+Ilaria-lp@users.noreply.github.com>
2025-02-27 18:24:37 +0100

    Update reshape-cut-rotate_v4.1.py
    added a check on x and y dimension

--- reshape-cut-rotate_v4.1.py
+++ reshape-cut-rotate_v4.1.py
@@ -15,14 +15,13 @@ import math
 import numpy as np
 import os
 
-from h5_map_handling import check_bms, count_steps, orientation
+from h5_map_handling import check_bms, count_steps, orientation, warning
 
 EXT = "h5"
 PATH_SCALAR = "/Measurement/TransientScalarData"
 PATH_VECTOR = "/Measurement/TransientVectorData"
 POSITIONERS = "/Measurement/Positioners"
 NEW_FOLDER = "/cut-reshaped/"
-NEWNAME_APP = "_reshaped.h5"
 I0_MONITOR = "/BMS-T-Average"
 
 # The motor positions copied to the output file are the following:
@@ -31,113 +30,115 @@ AcceptedList = ['BMS-3-Average','DIODE-Average','X','Y','Z']
 
 def cut_reshape(in_file, out_fold):
     
-    with h5py.File(in_file, 'r') as f:
-        move_ver = False
-        comment_line = ''
-        
-        for run in f.keys():
-            date_str = 'Run%Y%m%d %H%M%S'
-            # the first part of the string contains date and time of acquisition
-            date_acq = datetime.strptime(run[:18], date_str)     
-            # the rest of the string is the sample_name
-            sample_name = run[19:]
-            new_map = './'+ NEW_FOLDER + sample_name + date_acq.strftime('_%Y-%m-%d_%H-%M-%S')
+    f = h5py.File(in_file, 'r')
+    move_ver = False
+    comment_line = 'This file has been generated with the script reshape-cut-rotate_V4.1.\n'
     
-            print("Sample_name: %s." %sample_name)
-            print(date_acq.strftime('\tAcquisition time: %d/%m/%Y - %H:%M:%S.'))
+    for run in f.keys():
+        date_str = 'Run%Y%m%d %H%M%S'
+        # the first part of the string contains date and time of acquisition
+        date_acq = datetime.strptime(run[:18], date_str)     
+        # the rest of the string is the sample_name
+        sample_name = run[19:]
+        new_map = './'+ NEW_FOLDER + sample_name + date_acq.strftime('_%Y-%m-%d_%H-%M-%S')
+    
+        print("Sample_name: %s." %sample_name)
+        print(date_acq.strftime('\tAcquisition time: %d/%m/%Y - %H:%M:%S.'))
+        
+        try:                
+            x = np.array(f[run+PATH_SCALAR+"/X"][...])
+            y = np.array(f[run+PATH_SCALAR+"/Y"][...])
+        except KeyError:
+            warning('HOR/VER movement not found! Moving on!')
+            break
+        
+    # when X moves first, in order to see the map on PyMCA, the number of 
+    # columns to enter is the number of rows. The map will appear rotated.  
+    # The following section fixes this issue. The output file will be shown 
+    # in the correct orientation on PyMCA.
+        if len(x)==1 or len(y)==1:        
+            warning('This does not look like a map: is it a linear scan?')
+            break
+        if len(np.shape(x)) == 2:
+            warning('This maps seems to be 2D...no need for reshaping. Moving on!')
+            break
+        elif len(np.shape(x)) > 2 or len(np.shape(y)) >2:
+            warning('This maps seems to be have >2 dimensions. Moving on!')
+            break        
+        try:
+            x, y, move_ver = orientation(x, y)            
+        except ValueError:
+            break
+
+        shape_x, shape_y = count_steps(x), count_steps(y)
+        tot_points_raw = shape_x * shape_y
+
+        print("\tOriginal map shape: (%d, %d), points = %d." %(shape_x, shape_y, tot_points_raw) )
+
+        # check for beam dump on the BMS signal (i0)
+        bms = np.array(f[run+PATH_SCALAR+I0_MONITOR][...])
+        beam_lost, valid_pixels, comment_line = check_bms(bms, comment_line)
+        
+        if valid_pixels == 0:
+            warning('No valid pixels, moving on.')
+            break
+
+        if move_ver:
+            row = shape_x
+            col = math.floor(valid_pixels/shape_x)
+            new_map += '_rot'
+            comment_line += 'This map has been rotated.\n'
+        else:
+            col = shape_y
+            row = math.floor(valid_pixels/shape_y)
+                              
+        if valid_pixels < shape_x*shape_y:
+            print('    !\tValid pixels = %s out of %s' %(valid_pixels, shape_x*shape_y))
+            if not beam_lost:
+                print('\tIncomplete map collection found.')
+                comment_line += 'Incomplete acquisition found. The original map was cut to have a rectangular shape.'
+            print('\tNew map shape: (%d, %d)' %(row, col))
+            new_map += '_cut'           
+
+        if (row*col==shape_x*shape_y):
+            print('\tNo cutting, just reshaping.\n')
+            comment_line += 'This map was not cut. Only reshaping has been done.'
+            print('\tNew map shape: (%d, %d)' %(row, col))
             
-            try:                
-                x = f[run+PATH_SCALAR+"/X"][...]
-                y = f[run+PATH_SCALAR+"/Y"][...]
-            except KeyError:
-                print('\tx x x x x x x x x WARNING x x x x x x x x x')
-                print('\tx  HOR/VER movement not found! Moving on! x')
-                print('\tx x x x x x x x x x x x x x x x x x x x x x\n')
-                break
-            
-        # when X moves first, in order to see the map on PyMCA, the number of 
-        # columns to enter is the number of rows. The map will appear rotated.  
-        # The following section fixes this issue. The output file will be shown 
-        # in the correct orientation on PyMCA.
-            try:
-                x, y, move_ver = orientation(x, y)            
-            except ValueError:
-                print('\tx x x x x x x x x x x x x x x x x x x x x x x x')
-                print('\tx  Size not valid for map. Please check file. x ')
-                print('\tx x x x x x x x x x x x x x x x x x x x x x x x\n')
-                break
-
-            shape_x, shape_y = count_steps(x), count_steps(y)
-            tot_points_raw = shape_x * shape_y
-
-            print("\tOriginal map shape: (%d, %d), points = %d." %(shape_x, shape_y, tot_points_raw) )
-
-            # check for beam dump on the BMS signal (i0)
-            bms = f[run+PATH_SCALAR+I0_MONITOR][...]
-            beam_lost, valid_pixels = check_bms(bms)
-            
-            if valid_pixels == 0:
-                print('\tx x x x x x x x x x x x x x x x')
-                print('\tx No valid pixels, moving on. x')
-                print('\tx x x x x x x x x x x x x x x x\n')
-                break
-
+        new_map += '.h5'                              
+        fout =  h5py.File(new_map, 'w')
+        final_points = row * col
+        
+        fout.create_dataset("Comments", data=comment_line)
+ 
+        # Reshaping TransientVectorData
+        for vectorData in f[run+PATH_VECTOR].keys():
+            v = f[run+PATH_VECTOR+"/"+vectorData][...]
+            v = v[0:final_points]
+            v = v.reshape(row,col,v.shape[-1])                   
+            # the rotation of the map is done here:
             if move_ver:
-                row = shape_x
-                col = math.floor(valid_pixels/shape_x)
-                new_map += '_rot'
-                comment_line += 'This map has been rotated.\n'
-            else:
-                col = shape_y
-                row = math.floor(valid_pixels/shape_y)
-                                  
-            if valid_pixels < shape_x*shape_y:
-                print('    !\tValid pixels = %s out of %s' %(valid_pixels, shape_x*shape_y))
-                if not beam_lost:
-                    print('\tIncomplete map collection found.')
-                    comment_line += 'Incomplete map collection found'
-                print('\tNew map shape: (%d, %d)' %(row, col))
-                new_map += '_cut'           
-
-            if (row*col==shape_x*shape_y):
-                print('\tNo cutting, just reshaping.\n')
-                comment_line += 'This map was not cut. Only reshaping has been done.'
-                print('\tNew map shape: (%d, %d)' %(row, col))
+               v = np.rot90(v, -1, axes=(1,0))
+ 
+            fout.create_dataset(run+"/Detector_data/"+vectorData, data=v, compression = "gzip", shuffle=True)
+ 
+        # Reshaping TransientScalarData:')
+        for scalarData in f[run+PATH_SCALAR].keys():
+            if scalarData in AcceptedList or scalarData.endswith('LiveTime'):                
+                s = f[run+PATH_SCALAR+"/"+scalarData][...]
+                s = s[0:final_points]
+                if s.shape[0] == final_points:
+                    s = s.reshape(row, col)
                 
-            new_map += '.h5'                              
-            fout =  h5py.File(new_map, 'w')
-            final_points = row * col
-            
-            fout.create_dataset("Comments", data=comment_line)
- 
-            # Reshaping TransientVectorData
-            for vectorData in f[run+PATH_VECTOR].keys():
-                v = f[run+PATH_VECTOR+"/"+vectorData][...]
-                v = v[0:final_points]
-                v = v.reshape(row,col,v.shape[-1])                   
-                # the rotation of the map is done here:
                 if move_ver:
-                   v = np.rot90(v, -1, axes=(1,0))
+                    s = np.rot90(s, -1, axes=(1,0))
+                
+                fout.create_dataset(run+"/Motor_positions/"+scalarData, data=s)
  
-                fout.create_dataset(run+"/Detector_data/"+vectorData, data=v, compression = "gzip", shuffle=True)
- 
-            # Reshaping TransientScalarData:')
-            for scalarData in f[run+PATH_SCALAR].keys():
-                if scalarData in AcceptedList:
-                    s = f[run+PATH_SCALAR+"/"+scalarData][...]
-                    s = s[0:final_points]
-                    if s.shape[0] == final_points:
-                        s = s.reshape(row, col)
-                    
-                    if move_ver:
-                        s = np.rot90(s, -1, axes=(1,0))
-                    
-                    fout.create_dataset(run+"/Motor_positions/"+scalarData, data=s)
- 
-            # Reshaping Positioners (i.e. motor position before map collection)
-            for motor_position in f[run+POSITIONERS].keys():
-                p = f[run+POSITIONERS+"/"+motor_position][...]                   
-                fout.create_dataset(run+"/Starting_positions/"+motor_position, data=p)
+        # Reshaping Positioners (i.e. motor position before map collection)
+        for motor_position in f[run+POSITIONERS].keys():
+            p = f[run+POSITIONERS+"/"+motor_position][...]                   
+            fout.create_dataset(run+"/Starting_positions/"+motor_position, data=p)
 
 
 ####################################################################
